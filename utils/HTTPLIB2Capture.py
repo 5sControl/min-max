fe7cb47729bace820160e09b391953c4cece8222
eugenos-programos <elena.samchuk.73@mail.ru>
2023-05-16 14:00:24 +0300

    refactor(HTTPLIB2Capture.py): simplify constructor by using kwargs instead of explicit arguments feat(HTTPLIB2Capture.py): add error handling to get_snapshot method The constructor of HTTPLIB2Capture now uses kwargs instead of explicit arguments, which simplifies the code and makes it more flexible. The get_snapshot method now includes error handling to catch exceptions that may occur when retrieving an image from the camera URL.
    refactor(visualization.py): change argument names to conform to PEP8
    The argument names in the draw_rect_with_text function in visualization.py have been changed to conform to PEP8 naming conventions.

--- utils/HTTPLIB2Capture.py
+++ utils/HTTPLIB2Capture.py
@@ -6,52 +6,19 @@ h = httplib2.Http(".cache")
 
 
 class HTTPLIB2Capture:  # for inference
-    def __init__(self, path, img_size=640, stride=32, username='username', password='password'):
-        p = str(Path(path).absolute())  # os-agnostic absolute path
+    def __init__(self, path, **kwargs):
         self.camera_url = path
-        self.username = username
-        self.password = password
-        files = [path]
+        self.username = kwargs.get('username', None)
+        self.password = kwargs.get('password', None)
 
-        images = [path]
-        videos = []
-        ni, nv = len(images), len(videos)
-
-        self.img_size = img_size
-        self.stride = stride
-        self.files = images + videos
-        self.nf = ni + nv  # number of files
-        self.video_flag = [False] * ni + [True] * nv
-        self.mode = 'image'
-        if any(videos):
-            self.new_video(videos[0])  # new video
-        else:
-            self.cap = None
-
-    def __iter__(self):
-        self.count = 0
-        return self
-
-    def __next__(self):
+    def get_snapshot(self):
         try:
-            path = self.camera_url
-
-            self.count += 1
             h.add_credentials(self.username, self.password)
-            resp, content = h.request(path, "GET", body="foobar")
-
-            if resp.status == 200:
-                # Decode the JPEG image data into a NumPy array
-                nparr = np.frombuffer(content, np.uint8)
-                img0 = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
-            else:
-                print('Error: Could not retrieve image')
-            assert img0 is not None, 'Image Not Found ' + path
-
-            return path, None, img0, self.cap
-        except:
-            return None, None, None, None
-
-    def __len__(self):
-        return self.nf  # number of files
+            resp, content = h.request(self.camera_url, "GET", body="foobar")
+            nparr = np.frombuffer(content, np.uint8)
+            img0 = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+            assert img0 is not None, 'Image Not Found ' + self.camera_url
+            return self.camera_url, img0
+        except Exception as e:
+            print("Data exc - ", e)
+            return None, None
--- utils/visualization.py
+++ utils/visualization.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 
-def draw_rect_with_text(img:np.array, rect_coords:tuple, text:str, color, **rect_kwargs):
+def draw_rect_with_text(img: np.array, rect_coords: tuple, text: str, color, **rect_kwargs):
     x1, y1, x2, y2 = rect_coords
     image = cv2.rectangle(img, (x1, y1), (x2, y2), color, **rect_kwargs)
     cv2.putText(
@@ -10,8 +10,7 @@ def draw_rect_with_text(img:np.array, rect_coords:tuple, text:str, color, **rect
         text,
         (x1 + 30, y1 + 30),
         cv2.FONT_HERSHEY_SIMPLEX,
-        1.2,
+        1.0,
         color,
         2)
     return image
-
