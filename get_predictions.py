1c5f8c69d81d0a9f53626554e004ecf2ab5b8824
eugenos-programos <elena.samchuk.73@mail.ru>
2023-06-21 13:49:06 +0300

    fix(requests): fix requests sending to server

--- get_predictions.py
+++ get_predictions.py
@@ -3,33 +3,32 @@ import numpy as np
 import cv2
 
 
-PORT = 3000
+PORT = 8888
 
-def predict_human(img: np.array):
+def predict_human(img: np.array, server_url: str):
     cv2.imwrite("img.jpg", cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-    with open("img.jpg", 'rb') as img_file:
-        response = requests.post(
-            f"http://localhost:{PORT}/predict_human",
+    response = requests.post(
+            f"{server_url}:{PORT}/predict_human",
             files={
-                "image": img_file
+                "image": open("img.jpg", 'rb')
             }
         )
-        n_boxes = response.json().get('n_boxes')
-        coordinates = response.json().get("coordinates")
-        return [n_boxes, coordinates]
+    n_boxes = response.json().get('n_boxes')
+    coordinates = np.array(response.json().get("coordinates"))
+    return [n_boxes, coordinates]
     
 
-def predict_boxes(img: np.array):
-    cv2.imwrite("img.jpg", cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+def predict_boxes(img: np.array, server_url: str):
+    cv2.imwrite("images/img.jpg", cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
     with open("img.jpg", 'rb') as img_file:
         response = requests.post(
-            f"http://localhost:{PORT}/predict_boxes",
+            f"{server_url}:{PORT}/predict_boxes",
             files={
                 "image": img_file
             }
         )
         n_boxes = response.json().get('n_boxes')
-        coordinates = response.json().get("coordinates")
+        coordinates = np.array(response.json().get("coordinates"))
         return [n_boxes, coordinates]
 
 
