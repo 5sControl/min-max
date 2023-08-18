9cc40fe5d1df4a3719b76053a116667f004f767b
eugenos-programos <elena.samchuk.73@mail.ru>
2023-08-18 14:11:25 +0300

    refactor: move flask configs to main configs

--- confs/load_configs.py
+++ confs/load_configs.py
@@ -2,10 +2,7 @@ import json
 
 
 with open("confs/configs.json", "r") as conf:
-    configs = json.load(conf)
-    N_STEPS = configs.get("n_steps")
-    DEBUG_FOLDER = configs.get("debug_folder")
+    configs: dict = json.load(conf)
 
-#with open("model_image/flask_configs/flask_confs.json", "r") as flask_conf:
-    #configs = json.load(conf)
-    #PORT = configs.get("port")
+with open("model_image/flask_configs/flask_confs.json", "r") as flask_conf:
+    configs.update(json.load(flask_conf))
