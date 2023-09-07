8ba6d7c6123a4c901c1066928645f3ee722d4061
eugenos-programos <elena.samchuk.73@mail.ru>
2023-09-07 13:28:14 +0300

    feat: change httplib2 on requests

--- connection/ImageCapture.py
+++ connection/ImageCapture.py
@@ -1,8 +1,7 @@
 import numpy as np
 import cv2
 from logging import Logger
-import httplib2
-import time
+import requests
 
 
 class ImageCapture:
@@ -13,19 +12,13 @@ class ImageCapture:
         self._logger : Logger = kwargs.get('logger')
         self._server_url = kwargs.get("server_url")
         self._prev_img = None
-        self._conn = httplib2.Http(".cache")
         if not self._username or not self._password:
             self.logger.warning("Empty password or username")
 
     def get_snapshot(self) -> cv2.Mat:
-        self._conn.add_credentials(self._username, self._password)
         try:
-            resp, image = self._conn.request(
-                self._camera_ip, 
-                "GET", 
-                body="foobar"
-            )
-            img_array = np.frombuffer(image, np.uint8)
+            resp = requests.get(self._camera_ip)
+            img_array = np.frombuffer(resp.content, np.uint8)
             image = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
             return image
         except Exception as exc:
